f0675db21db497bfcd1fdb70e72c3d5157b970b4
Akash Soni <akashsoni.6898@gmail.com>
2021-07-06 01:43:29 +0530

    added onboarding screen

---
+++ lib/customColors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static Color blue = Color.fromRGBO(100, 179, 253, 1);
+  static Color white = Color.fromRGBO(251, 252,254, 1);
+  static Color black = Color.fromRGBO(25,56,83, 1);
+  static Color grey = Color.fromRGBO(169,169,169, 1);
+}
---
+++ lib/textValues.dart
@@ -0,0 +1,35 @@
+class TextValues{
+  static List<String> images = [
+    "images/page1.png",
+    "images/page2.png",
+    "images/page3.png",
+    "images/page4.png",
+  ];
+
+  static List<String> text1 = [
+    "Explore like a",
+    "Choose like a",
+    "Prepare for the",
+    "How can we",
+  ];
+
+  static List<String> text2 = [
+    "Ninja!",
+    "Pro!",
+    "Real world!",
+    "Help ? 🤚🏻",
+  ];
+
+  static List<String> images2 = [
+    "images/ninja.png",
+    "images/ok.png",
+    "images/building.png",
+  ];
+
+  static List<String> text3 = [
+    "Take your career to next level with personalized career info based on your interests, skill & personality.",
+    "No matter what aspect of college life you are looking for, we provide listings and rankings based on your preferences.",
+    "We'll show you exactly what to do to make your dream job a reality",
+    "Get ready for your transition from school to college and finally to a perfect career with us!",
+  ];
+}
